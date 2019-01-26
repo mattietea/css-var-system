a7339d409fb1db1b073a6cc1b6b3066507dd30a3
Matt Thomas <mattcthomas@me.com>
2019-01-26 14:23:24 -0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { bg, fontColor, fontFamily, ml, mr, mt } from './create-prop';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-primary: #3c40c6;
+    --color-secondary: #d2dae2;
+    --color-success: #05c46b;
+    --color-danger: #ff5e57;
+    --color-light: #fff;
+    --color-dark: #1e272e;
+
+    --font-family-sans: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+
+    --space-scale: 4px;
+    --font-size-scale: 4px;
+    --radii-scale: 2px;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+interface BoxProps {
+  bg?: string;
+  fontColor?: string;
+  fontFamily?: string;
+  ml?: number | string;
+  mr?: number | string;
+  mt?: number | string;
+}
+
+const Box = styled('div')<BoxProps>`
+  ${bg}
+  ${fontColor}
+  ${fontFamily}
+  ${ml}
+  ${mr}
+  ${mt}
+`;
+
+Box.defaultProps = {
+  bg: 'primary',
+  fontFamily: 'sans',
+};
+
+const Button = styled(Box)`
+  height: 200px;
+  width: 200px;
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Box bg="danger">
+        <Button
+          fontColor="light"
+          mt="calc(50vh - 100px)"
+          mr="auto"
+          ml="auto"
+          bg="success"
+        >
+          My Box
+        </Button>
+      </Box>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
